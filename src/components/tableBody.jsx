9aba526df5b19654292092d3d846002743557b46
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-04 18:05:25 -0400

    added buildpack

--- src/components/tableBody.jsx
+++ src/components/tableBody.jsx
@@ -3,8 +3,7 @@ import './main.css'
 
 const TableBody = props => {
     return (
-        <tbody className="text-info">
-            
+        <tbody className="text-info"> 
             {props.users ? props.users.map((user, i) =>(<tr key={user.login.uuid}>
                         <th scope="row">{i + 1}</th>
                         <td>{`${user.name.first} ${user.name.last}`}</td>
